29b2131f1c867b9ab487904f4e0d3b35c1186fed
Eica <33784474+eicaabreu@users.noreply.github.com>
2022-06-21 19:04:52 -0400

    Agregando archivo de base de datos

---
+++ TEST.Infrastructure/Database/1_init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE permission_types (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    description varchar(100) NOT NULL
+);
+
+
+CREATE TABLE permissions (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    permission_type_id INT NOT NULL,
+    date varchar(100) NOT NULL,
+    CONSTRAINT fk_permission_type_id FOREIGN KEY(permission_type_id) REFERENCES permission_types(id)
+    
+);
+
+INSERT INTO permission_types ( description) VALUES
+    ('Enfermedad')
+   ,('Diligencias')
+   ,('Otros')
+    
