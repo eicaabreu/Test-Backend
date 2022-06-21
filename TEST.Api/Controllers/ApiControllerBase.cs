d13100c82cc69194dab0886fa046d636f18f6539
Eica <33784474+eicaabreu@users.noreply.github.com>
2022-06-21 15:59:58 -0400

    Agregar archivos de proyecto.

---
+++ TEST.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TEST.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiControllerBase : ControllerBase
+    {
+        private IMediator _mediator = null!;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+    }
+}
